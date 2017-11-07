1cfad1e0bc1bfd3e47408738c67d1559966f2efd
Jonathan Goodwin <njo.goodwin@gmail.com>
2017-11-06 21:11:12 -0800

    Project Setup

---
+++ app/src/main/java/com/jgoodwin/shazamclone/DiscoverActivity.kt
@@ -0,0 +1,12 @@
+package com.jgoodwin.shazamclone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DiscoverActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_discover)
+    }
+}
